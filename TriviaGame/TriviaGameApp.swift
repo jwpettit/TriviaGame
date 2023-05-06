5f35a522349d5965dccd1851b2b0ad6a0cad7492
jwpettit <jwpettit@me.com>
2023-05-06 13:35:17 -0700

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Jeffrey Pettit on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
