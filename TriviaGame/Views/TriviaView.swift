f7f12f7f61c3a61b5e01cbfd5163ede574b5d8c9
jwpettit <jwpettit@me.com>
2023-08-13 18:13:31 -0700

    Made home page button navigate to question

---
+++ TriviaGame/Views/TriviaView.swift
@@ -0,0 +1,20 @@
+//
+//  TriviaView.swift
+//  TriviaGame
+//
+//  Created by Jeffrey Pettit on 8/13/23.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        QuestionView()
+    }
+}
+
+struct TriviaView_Previews: PreviewProvider {
+    static var previews: some View {
+        TriviaView()
+    }
+}
