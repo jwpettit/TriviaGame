d56b6bab243575e54c3cc8fa2fb2285e365d1a21
jwpettit <jwpettit@me.com>
2023-08-16 12:02:47 -0700

    Trivia Manager complete

--- TriviaGame/TriviaManager.swift
+++ TriviaGame/TriviaManager.swift
@@ -6,10 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
 
 class TriviaManager: ObservableObject {
     private(set) var trivia: [Trivia.Result] = []
     @Published private(set) var length = 0
+    @Published private(set) var index = 0
+    @Published private(set) var reachedEnd = false
+    @Published private(set) var answerSelected = FALSE
+    @Published private(set) var question: AttributedString = ""
+    @Published private(set) var answerChoices: [Answer] = []
+    @Published private(set) var progress: CGFloat = 0.00
+    @Published private(set) var score = 0
     
     init() {
         Task.init{
@@ -34,10 +42,37 @@ class TriviaManager: ObservableObject {
             DispatchedQueue.main.async {
                 self.trivia = decodedData.results
                 self.length = self.trivia.count
+                self.setQuestion()
             }
                 
         } catch {
             print("Error fetching trivia: \(error)")
+        }
+        
+    func goToNextQuestion() {
+        if index + 1 < length {
+            index += 1
+            setQuestion()
+        } else {
+            reachedEnd = true
+        }
+    }
+        
+    func setQuestion() {
+        answerSelected = false
+        progress = CGFloat(Double(index + 1) / Double(length) * 350)
+        
+        if index < length {
+            let currentTriviaQuestion = trivia[index]
+        question = currentTriviaQuestion.formattedQuestion
+            answerChoices = currentTriviaQuestion.answers
+        }
+    }
+        
+    func selectAnswer(answer: Answer) {
+        answerSelected = true
+        if answer.isCorrect {
+            score += 1
         }
     }
 }
