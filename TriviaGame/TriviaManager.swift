cb8fa04ea162ecbd4756c24960967de6eb09e93c
jwpettit <jwpettit@me.com>
2023-08-16 11:36:11 -0700

    Built Trivia file to format JSON data coming in from API

---
+++ TriviaGame/TriviaManager.swift
@@ -0,0 +1,12 @@
+//
+//  TriviaManager.swift
+//  TriviaGame
+//
+//  Created by Jeffrey Pettit on 8/16/23.
+//
+
+import Foundation
+
+class TriviaManager: ObservableObject {
+    
+}
--- TriviaGame/Views/QuestionView.swift
+++ TriviaGame/Views/QuestionView.swift
@@ -42,6 +42,7 @@ struct QuestionView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red: 0.984313725490196, green:
                             0.9294117647058824, blue: 0.8470588235294118))
+        .navigationBarHidden(true)
     }
 }
 
